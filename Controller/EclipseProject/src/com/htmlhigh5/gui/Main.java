0f7a198d12f7239b1cc7447268a15564d2ac98cc
Ian Andersen <ianandersen98@gmail.com>
2017-04-11 14:47:40 -0700

    Network stuff

--- Controller/EclipseProject/src/com/htmlhigh5/gui/Main.java
+++ Controller/EclipseProject/src/com/htmlhigh5/gui/Main.java
@@ -1,9 +1,19 @@
 package com.htmlhigh5.gui;
 
 import com.htmlhigh5.debug.Debug;
+import com.htmlhigh5.network.BadPacketSizeException;
+import com.htmlhigh5.network.PinCommand;
 
 public class Main {
 	public static void main(String[] args){
-		Debug.init();
+		//Debug.init();
+		for(int i = 0; i <= 100; i++){
+			try {
+				PinCommand c = new PinCommand(i);
+			} catch (BadPacketSizeException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 }
--- Controller/EclipseProject/src/com/htmlhigh5/network/Transmitter.java
+++ Controller/EclipseProject/src/com/htmlhigh5/network/Transmitter.java
@@ -9,15 +9,17 @@ import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 
+import com.sun.xml.internal.ws.api.message.Packet;
+
 public class Transmitter {
-	public static void main(String[] args){
-		try {
-			udpSendReceive();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+//	public static void main(String[] args){
+//		try {
+//			udpSendReceive();
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//	}
 	
 	public static int sendPacket() throws IOException{
 		byte[] buffer = {10,23,12,31,43,32,24};
@@ -32,20 +34,43 @@ public class Transmitter {
         return 0;
 	}
 	
-	public static void udpSendReceive() throws IOException{
-      DatagramSocket clientSocket = new DatagramSocket();
-      InetAddress IPAddress = InetAddress.getLocalHost();
-      byte[] sendData = new byte[1024];
-      byte[] receiveData = new byte[1024];
-      String sentence = "TEST";
-      sendData = sentence.getBytes();
-      DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9999);
-      clientSocket.send(sendPacket);
-      
-      DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-      clientSocket.receive(receivePacket);
-      String modifiedSentence = new String(receivePacket.getData());
-      System.out.println("FROM SERVER:" + modifiedSentence);
-      clientSocket.close();
+	public static int sendControlPacket(ControlPacket packet) throws IOException{
+		DatagramSocket clientSocket = new DatagramSocket();
+	      InetAddress IPAddress = InetAddress.getLocalHost();
+	      byte[] data = toBytes(packet.getData());
+	      DatagramPacket sendPacket = new DatagramPacket(data, data.length, IPAddress, 9999);
+	      clientSocket.send(sendPacket);
+	      clientSocket.close();
+		return 0;
 	}
+	
+//	public static void udpSendReceive() throws IOException{
+//      DatagramSocket clientSocket = new DatagramSocket();
+//      InetAddress IPAddress = InetAddress.getLocalHost();
+//      byte[] sendData = new byte[1024];
+//      byte[] receiveData = new byte[1024];
+//      String sentence = "TEST";
+//      sendData = sentence.getBytes();
+//      DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9999);
+//      clientSocket.send(sendPacket);
+//      
+//      DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+//      clientSocket.receive(receivePacket);
+//      String modifiedSentence = new String(receivePacket.getData());
+//      System.out.println("FROM SERVER:" + modifiedSentence);
+//      clientSocket.close();
+//	}
+	
+	public static byte[] toBytes(boolean[] input) {
+	    byte[] toReturn = new byte[input.length / 8];
+	    for (int entry = 0; entry < toReturn.length; entry++) {
+	        for (int bit = 0; bit < 8; bit++) {
+	            if (input[entry * 8 + bit]) {
+	                toReturn[entry] |= (128 >> bit);
+	            }
+	        }
+	    }
+
+	    return toReturn;
+	} 
 }
