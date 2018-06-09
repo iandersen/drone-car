7413de59d6d634e5fd43d89edb714fe504f8c9c8
Ian Andersen <ianandersen98@yahoo.com>
2018-06-09 20:30:27 +0000

    Streaming Working Now

--- ClientSide_Python/stream_start.sh
+++ ClientSide_Python/stream_start.sh
@@ -1,1 +1,1 @@
-cvlc -A alsa,none --alsa-audio-device default v4l2:///dev/video0 --v4l2-width 480 --v4l2-height 340 --v4l2-chroma MJPG --v4l2-hflip 1 --v4l2-vflip 1 --sout '#transcode{fps=10}:standard{access=http{mime=multipart-mixed-replace},mux=flv,dst=:8554/}' -I dummy &
+/usr/bin/cvlc -A alsa,none --alsa-audio-device default v4l2:///dev/video0 --v4l2-width 480 --v4l2-height 340 --v4l2-chroma MJPG --v4l2-hflip 1 --v4l2-vflip 1 --sout '#transcode{fps=10}:standard{access=http{mime=multipart-mixed-replace},mux=ts,dst=:8554/}' -I dummy &
